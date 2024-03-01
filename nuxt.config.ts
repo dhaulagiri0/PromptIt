e1a8d0e049247eb3912c4aeed6d63c1613d547ca
Sushant <sushant.pulavarthi09@gmail.com>
2024-03-01 23:27:12 +0000

    Setup Firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,5 +32,52 @@ export default defineNuxtConfig({
       'composables/*/index.{ts,js,mjs,mts}',
       'composables/**'
     ]
-  }
+  },
+  runtimeConfig: {
+    public: {
+      firebase: {
+        apiKey: process.env.FIREBASE_APIKEY,
+        authDomain: process.env.FIREBASE_AUTHDOMAIN,
+        projectId: process.env.FIREBASE_PROJECTID,
+        storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+        appId: process.env.FIREBASE_APPID,
+      },
+      webhook_url: process.env.WEBHOOK_URL
+    }
+  },
+  hooks: {
+    'pages:extend'(pages) {
+      const pagesToRemove: NuxtPage[] = [];
+      pages.forEach(page => {
+        if (page.path.includes('component')) pagesToRemove.push(page);
+      });
+
+      pagesToRemove.forEach((page: NuxtPage) => {
+        pages.splice(pages.indexOf(page), 1);
+      });
+    }
+  },
+  // app: {
+  //   head: {
+  //     link: [
+  //       {
+  //         rel: 'preconnect',
+  //         href: 'https://fonts.googleapis.com'
+  //       },
+  //       {
+  //         rel: 'preconnect',
+  //         href: 'https://fonts.gstatic.com'
+  //       },
+  //       {
+  //         rel: 'stylesheet',
+  //         href: 'https://fonts.googleapis.com/css2?family=poppins:wght@200;300;400;600;700;800&display=swap'
+  //       },
+  //       {
+  //         rel: 'icon',
+  //         href: '/favicon.ico',
+  //       }
+  //     ]
+  //   }
+  // }
 })
