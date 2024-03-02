146b4c3f8191e8dd40079bba1cb754879d5343b1
Kevin Wang <yiqinw4@gmail.com>
2024-03-02 21:33:10 +0000

    index wip

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,30 @@
 </script>
 
 <template>
-  <div class="bg-slate-900 h-dvh p-5">
-    <Header />
-    <div class="main-content flex space-x-10 ml-28 mr-28">
-      <div class="left flex-1 p-4">
+  <div class="bg-slate-900 h-dvh p-5 grid content-center">
+    <!-- <Header /> -->
+    <div class="main-content flex space-x-10 ml-28 mr-28 ">
+      <div class="left flex-1 basis-2/3 p-4">
         <WindowCard class="w-fill" header-color="richblue">
           <div class="p-10">
             <IconsMainLogo class="w-fill min-w-20" />
           </div>
         </WindowCard> 
       </div>
-      <div class="right flex-none p-4">
-        <WindowCard class="">
-          <ConsoleField place/>
-          <ThiccButton />
-          <ThiccButton />
+      <div class="right flex-1 basis-1/3 p-4">
+        <WindowCard>
+          <div class="p-5 space-y-5">
+            <ConsoleField placeholder-text="Game Code_"/>
+            <ThiccButton>
+              <h3>Join a Game</h3>
+            </ThiccButton>
+            <ThiccButton color="offwhite">
+              <h3>Start a Game</h3>
+            </ThiccButton>
+          </div>
         </WindowCard>
       </div>
     </div>
+
   </div>
 </template>
