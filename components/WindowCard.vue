5b0e134cb1b8313014127464023aab07b2547f28
Kevin Wang <yiqinw4@gmail.com>
2024-03-02 03:15:40 +0000

    added gohu font and WindoCard component

---
+++ components/WindowCard.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+
+<template>
+  <div class="
+    container 
+    mx-auto 
+    main-container 
+    rounded-vl
+    w-max 
+    border-4
+    bg-offwhite
+    border-black
+    overflow-hidden
+    drop-shadow-solid"
+  >
+    <div class="block">
+      <div class="header space-x-4 text-right p-2 border-b-4 border-black bg-grapefruit">
+        <img class="inline text-l" src="/assets/svgs/ui-circle.svg"/>
+        <img class="inline text-l" src="/assets/svgs/ui-dash.svg"/>
+        <img class="inline text-l" src="/assets/svgs/ui-x.svg"/>
+        <!-- <span class="text-3xl">minus </span> -->
+        <!-- <span class="text-3xl">cross</span> -->
+      </div>
+      <div class="content-container p-4">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,21 @@ module.exports = {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    borderRadius: {
+      'vl' : '1rem',
+    },
+    extend: {
+      fontFamily: {
+        gohu: ['Gohu', ],
+      },
+      colors: {
+        'grapefruit': '#EBA166',
+        'offwhite': '#EAE1D8',
+      },
+      dropShadow: {
+        'solid': '-5px 5px 0px rgba(0, 0, 0, 1.0)',
+      },
+    },
   },
   plugins: [],
 }
