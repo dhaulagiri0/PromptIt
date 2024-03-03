7a0b5a8e899666820ffdd074a8f0d2b226df1563
Sushant <sushant.pulavarthi09@gmail.com>
2024-03-03 02:03:14 +0000

    Basic Lobby routing

--- composables/useTypes.ts
+++ composables/useTypes.ts
@@ -3,3 +3,11 @@ export type Message = {
   user: string | undefined;
   createdAt: string | undefined;
 }
+
+export type Game = {
+  gameId: string;
+  host: string;
+  players: string[];
+  antagonistId: string;
+  state: string;
+}
---
+++ pages/lobby/[id].vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    id="login"
+    class="bg-gradient-to-tr from-richpink to-richblue h-screen font-gohu overflow-x-hidden flex flex-col">
+    <Header />
+    <main class="grow flex flex-col justify-center">
+    </main>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+const { createGame } = useLobby()
+const router = useRouter()
+const route = useRoute()
+
+const gameId = route.params.id;
+
+</script>
