b95f02e5b78ce0af7eeac897391e662b7882a2de
Kevin Wang <yiqinw4@gmail.com>
2024-03-03 21:57:40 +0000

    basic storage for game state

--- stores/game.ts
+++ stores/game.ts
@@ -5,21 +5,64 @@ import {
   } from 'firebase/firestore'
 
 export const useGameStore = defineStore('game_states', () => {
-    const hostId = ref("")
-    const gameId = ref("")
-    const antagonistId = ref("")
-    const aiSeshId = ref("")
-    const gameStatus = ref("lobby")
-    const generalTasks = ref({})
-    const indiv = ref({})
-    const players = ref([])
+    const hostId = ref("");
+    const gameId = ref("");
+    const antagonistId = ref("");
+    const aiSeshId = ref("");
+    const gameStatus = ref("waiting");
+    const generalTasks = ref({});
+    const indivTasks = ref({});
+    const players = ref([]);
 
+    function setGameId(_gameId: string) {
+      gameId.value = _gameId;
+    }
+
+    function setHostId(_hostId: string) {
+      gameId.value = _hostId;
+    }
+
+    // should only be used with subscribeGameState from useGameListeners
     function updateState(gameData: DocumentData) {
-        console.log(gameData)
-        console.log("we updating")
-        hostId.value = gameData["hostId"]
+        console.log(gameData);
+        console.log("we updating");
+        gameId.value = gameData["gameId"];
+        hostId.value = gameData["hostId"];
+        antagonistId.value = gameData["antagonistId"];
+        aiSeshId.value = gameData["aiSeshId"];
+        gameStatus.value = gameData["state"];
+        players.value = gameData["players"];
+        generalTasks.value = gameData["generalTasks"];
+        indivTasks.value = gameData["indivTasks"];
+
+        console.log(antagonistId.value)
+        console.log(generalTasks.value)
+    }
+
+    // clear all refs after game finishes
+    function clearGameState() {
+      hostId.value = "";
+      gameId.value = "";
+      antagonistId.value = "";
+      aiSeshId.value = "";
+      gameStatus.value = "waiting";
+      generalTasks.value = {};
+      indivTasks.value = {};
+      players.value = [];
     }
 
   
-    return { hostId, gameId, updateState }
+    return { 
+      hostId, 
+      gameId, 
+      antagonistId, 
+      aiSeshId,
+      gameStatus,
+      generalTasks,
+      indivTasks,
+      players,
+      setGameId,
+      setHostId, 
+      updateState,
+      clearGameState, }
   })
