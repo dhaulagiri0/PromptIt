70f07dc01864984ef105a8e2fb7811e5e30b4261
Kevin Wang <yiqinw4@gmail.com>
2024-03-08 23:38:32 +0000

    tweaked chat ui

--- pages/game/[id].vue
+++ pages/game/[id].vue
@@ -2,118 +2,119 @@ l<template>
     <div class="bg-richblue h-screen font-gohu overflow-x-hidden flex flex-col">
         <Header back-visible="true" />
         <main class="grow flex flex-row justify-center space-x-4 px-4 pb-4">
-            <WindowCard class="flex flex-col basis-1/4 h-full" headerText="Team Chat">
-                <div id="con" class="h-full space-y-4">
-                    <div>
-                        <div 
-                            :key="allMessages"
-                            id="messagebox" 
-                            class="
-                            overflow-y-scroll
-                            no-scrollbar
-                            scrolling-auto
-                            h-[500px]">
-                            <div>
-                                <div v-for="(message, index) in allMessages" :key="message">
-                                    <div class="
-                                        container
-                                        flex
-                                        items-center
-                                        "
-                                    >
-                                        <div class="flex-1 space-y-2 mb-4 w-fill grid">
-                                            <div class="
-                                                border-white
-                                                rounded-vl
-                                                overflow-hidden
-                                                p-3
-                                                pl-5
-                                                pr-5
-                                                bg-black
-                                                border-4
-                                                text-offwhite
-                                                font-gohu
-                                                text-s
-                                                max-w-48
-                                                text-nowrap
-                                                text-ellipsis
-                                                "
-                                                v-if="(index == 0 && user != null && message.sentBy != user.uid) || (user != null && message.sentBy != user.uid && allMessages[index - 1] != undefined && allMessages[index - 1].sentBy != message.sentBy)"
-                                                >
-                                                {{ message.userName }}
+            <div class="flex flex-col basis-1/4 h-fill space-y-4">
+                <WindowCard class="flex-1 flex-col" headerText="Team Chat">
+                    <div id="con" class="h-full">
+                        <div class="h-fill">
+                            <div 
+                                :key="allMessages"
+                                id="messagebox" 
+                                class="
+                                overflow-y-scroll
+                                no-scrollbar
+                                scrolling-auto
+                                h-5/6">
+                                <div>
+                                    <div v-for="(message, index) in allMessages" :key="message">
+                                        <div class="
+                                            container
+                                            flex
+                                            items-center
+                                            "
+                                        >
+                                            <div class="flex-1 space-y-2 mb-4 w-fill grid">
+                                                <div class="
+                                                    border-grapefruit
+                                                    rounded-vl
+                                                    overflow-hidden
+                                                    p-3
+                                                    pl-5
+                                                    pr-5
+                                                    bg-black
+                                                    border-4
+                                                    text-offwhite
+                                                    font-gohu
+                                                    text-s
+                                                    max-w-48
+                                                    text-nowrap
+                                                    text-ellipsis
+                                                    "
+                                                    v-if="(index == 0 && user != null && message.sentBy != user.uid) || (user != null && message.sentBy != user.uid && allMessages[index - 1] != undefined && allMessages[index - 1].sentBy != message.sentBy)"
+                                                    >
+                                                    {{ message.userName }}
+                                                </div>
+                                                <div class="
+                                                    w-fit
+                                                    rounded-vl
+                                                    overflow-hidden
+                                                    p-3
+                                                    pl-5
+                                                    pr-5
+                                                    text-offwhite
+                                                    font-gohu
+                                                    text-s
+                                                    "
+                                                    :class="`${user != null && message.sentBy != user.uid ? 'bg-black' : 'justify-self-end bg-richblue'}`"                                                >
+                                                    {{ message.text }}
+                                                </div>
+                                                <!-- <div class="
+                                                    w-fit
+                                                    rounded-vl
+                                                    overflow-hidden
+                                                    p-3
+                                                    pl-5
+                                                    pr-5
+                                                    text-offwhite
+                                                    font-gohu
+                                                    text-s
+                                                    bg-richblue
+                                                    justify-self-end
+                                                    "
+                                                    v-else
+                                                    >
+                                                    {{ message.text }}
+                                                </div> -->
                                             </div>
-                                            <div class="
-                                                w-fit
-                                                rounded-vl
-                                                overflow-hidden
-                                                p-3
-                                                pl-5
-                                                pr-5
-                                                text-offwhite
-                                                font-gohu
-                                                text-s
-                                                "
-                                                :class="`${user != null && message.sentBy != user.uid ? 'bg-black' : 'justify-self-end bg-richblue'}`"                                                >
-                                                {{ message.text }}
-                                            </div>
-                                            <!-- <div class="
-                                                w-fit
-                                                rounded-vl
-                                                overflow-hidden
-                                                p-3
-                                                pl-5
-                                                pr-5
-                                                text-offwhite
-                                                font-gohu
-                                                text-s
-                                                bg-richblue
-                                                justify-self-end
-                                                "
-                                                v-else
-                                                >
-                                                {{ message.text }}
-                                            </div> -->
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </WindowCard>
+                <form class="w-fill flex-1/5 flex space-x-2" @submit.prevent="handleSendMessage">
+                    <input class="
+                        flex-1        
+                        basis-2/3            
+                        border-black
+                        rounded-vl
+                        overflow-hidden
+                        p-3
+                        pl-5
+                        pr-5
+                        bg-offwhite
+                        w-fill
+                        placeholder:text-richgrey
+                        border-4" placeholder="message"
+                        v-model="newMessage"/>
 
-                    <form class="w-fill flex space-x-2" @submit.prevent="handleSendMessage">
-                        <input class="
-                            flex-1        
-                            basis-2/3            
-                            border-black
-                            rounded-vl
-                            overflow-hidden
-                            p-3
-                            pl-5
-                            pr-5
-                            bg-offwhite
-                            w-fill
-                            placeholder:text-richgrey
-                            border-4" placeholder="message"
-                            v-model="newMessage"/>
-
-                        <button class="
-                            flex-1
-                            basis-1/3
-                            border-black
-                            rounded-vl
-                            overflow-hidden
-                            p-3
-                            pl-5
-                            pr-5
-                            w-fill
-                            border-4 
-                            bg-grapefruit"
-                            type="submit">  
-                            Send    
-                        </button>    
-                    </form>    
-                </div>
-            </WindowCard>
+                    <button class="
+                        flex-1
+                        basis-1/3
+                        border-black
+                        rounded-vl
+                        overflow-hidden
+                        p-3
+                        pl-5
+                        pr-5
+                        w-fill
+                        border-4 
+                        bg-grapefruit"
+                        type="submit">  
+                        Send    
+                    </button>    
+                </form>    
+            </div>
             <div class="
                 flex-1 basis-2/4
                 border-white
